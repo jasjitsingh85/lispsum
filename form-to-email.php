75297e4bef2f1ca5a2c3f95b6899db3c541e6ff2
Jasjit Singh <jazjit.singh@gmail.com>
2014-01-17 00:12:17 -0500

    mail update

--- form-to-email.php
+++ form-to-email.php
@@ -1,29 +1,29 @@
 <?php
 require_once('class.phpmailer.php');
 
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$company = $_POST['company'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
+// if(!isset($_POST['submit']))
+// {
+// 	//This page should not be accessed directly. Need to submit the form.
+// 	echo "error; you need to submit the form!";
+// }
+// $name = $_POST['name'];
+// $visitor_email = $_POST['email'];
+// $company = $_POST['company'];
+// $phone = $_POST['phone'];
+// $message = $_POST['message'];
 
-//Validate first
-if(empty($name)||empty($visitor_email)) 
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
+// //Validate first
+// if(empty($name)||empty($visitor_email)) 
+// {
+//     echo "Name and email are mandatory!";
+//     exit;
+// }
 
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
+// if(IsInjected($visitor_email))
+// {
+//     echo "Bad email value!";
+//     exit;
+// }
 
 
 
@@ -51,41 +51,41 @@ $mail->AddAddress("jazjit.singh@gmail.com");
     }
 
 
-$email_from = 'jeffreymames@gmail.com';//<== update the email address
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name. Their phone number is $phone.  They work at $company\n".
-    "Here is the message:\n $message".
+// $email_from = 'jeffreymames@gmail.com';//<== update the email address
+// $email_subject = "New Form submission";
+// $email_body = "You have received a new message from the user $name. Their phone number is $phone.  They work at $company\n".
+//     "Here is the message:\n $message".
     
-$to = "jazjit.singh@gmail.com";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: index.html');
+// $to = "jazjit.singh@gmail.com";//<== update the email address
+// $headers = "From: $email_from \r\n";
+// $headers .= "Reply-To: $visitor_email \r\n";
+// //Send the email!
+// mail($to,$email_subject,$email_body,$headers);
+// //done. redirect to thank-you page.
+// header('Location: index.html');
 
 
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
+// // Function to validate against any email injection attempts
+// function IsInjected($str)
+// {
+//   $injections = array('(\n+)',
+//               '(\r+)',
+//               '(\t+)',
+//               '(%0A+)',
+//               '(%0D+)',
+//               '(%08+)',
+//               '(%09+)'
+//               );
+//   $inject = join('|', $injections);
+//   $inject = "/$inject/i";
+//   if(preg_match($inject,$str))
+//     {
+//     return true;
+//   }
+//   else
+//     {
+//     return false;
+//   }
+// }
    
 ?> 
